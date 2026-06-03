9e0a3ee72f0273fa458de093abefa491a29c0c96
Lullka <lukaflavio@gmail.com>
2026-06-02 23:21:01 -0300

    gh-pgs update path route release

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  /* base: "/pizzariadosenai/", */
+  base: "/pizzariadosenai/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
